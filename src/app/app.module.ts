735181003230793f8392384613b63511a68a6948
ngocmai138 <nnm0891@gmail.com>
2024-10-29 11:36:52 +0700

    add material, ng-bootstrap, play-game component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserHistoryComponent } from './user-history/user-history.component';
+import {PlayGameComponent} from "./play-game/play-game.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserHistoryComponent
+    UserHistoryComponent,
+    PlayGameComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
