c5f14dbf666857cfdc21fbc30f95dbc551ab3075
MSI\nguye <vunqhe173069@fpt.edu.vn>
2024-10-29 09:29:07 +0700

    create component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserHistoryComponent } from './user-history/user-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserHistoryComponent
   ],
   imports: [
     BrowserModule,
