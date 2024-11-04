2ee8608df843852e14b47f2f7efbabcc6834755f
ngocmai138 <nnm0891@gmail.com>
2024-11-04 14:01:21 +0700

    update play component

--- src/app/interfaces/game.ts
+++ src/app/interfaces/game.ts
@@ -1,19 +1,36 @@
+export interface IPrize {
+  id: number;
+  name: string;
+  quantity: number;
+  format: string,
+  price: number,
+  totalPrice: number,
+  remainingPrice: number,
+  image: string,
+  colorCode: string,
+  gameId: number,
+  gameName: string
+}
+
+// GameResponse interface
 export interface IGame {
-  nameGame: string,
-  backgroundImage: string,
-  fontType: string,
-  fontSize: number,
-  borderColor: string,
-  prizesPerDay: number,
-  buttonColor: string,
-  spinStartTime: string,
-  spinEndTime: string,
-  timeSpin: number,
-  audioUrl: string,
-  namePromotion: string,
-  status: boolean,
-  createdAt: string,
-  startDate: string,
-  endDate: string,
-  disabled: boolean
+  id: number;
+  nameGame: string;
+  backgroundImage: string;
+  fontType: string;
+  fontSize: number;
+  borderColor: string;
+  prizesPerDay: number;
+  buttonColor: string;
+  spinEndTime: string; // LocalTime có thể được trả về dưới dạng chuỗi "HH:mm:ss"
+  spinStartTime: string; // LocalTime cũng sẽ ở định dạng chuỗi
+  timeSpin: number;
+  audioUrl: string;
+  namePromotion: string;
+  status: boolean;
+  createdAt: string; // LocalDate có thể được trả về dưới dạng chuỗi "yyyy-MM-dd"
+  startDate: string; // Tương tự cho LocalDate
+  endDate: string; // Tương tự cho LocalDate
+  disabled: boolean;
+  prizes: IPrize[]; // Danh sách giải thưởng
 }
