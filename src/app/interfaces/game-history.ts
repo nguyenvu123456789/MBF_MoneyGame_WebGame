59368866bb9271efa1b28f07dde501dc4bf6d6c6
TraNV <nguyentra2444@gmail.com>
2024-11-04 13:47:52 +0700

    update service

---
+++ src/app/interfaces/game-history.ts
@@ -0,0 +1,12 @@
+export interface IGameHistory {
+  id: number;
+  userId: number;
+  msisdn: string;
+  gameId: number;
+  gameName: string;
+  prizeId: number;
+  prizeName: string;
+  taskLoginId: number;
+  score: string;
+  taskTransactionId: number;
+}
--- src/app/services/data/game-history.service.ts
+++ src/app/services/data/game-history.service.ts
@@ -1,22 +1,29 @@
 import { Injectable } from '@angular/core';
-import {DataService} from "../data.service";
-import {IRequestPage} from "../../interfaces/request-page";
-import {IReceivedPrize} from "../../interfaces/received-prizes";
-import {IResponsePage} from "../../interfaces/response-page";
-import {IUserPlay} from "../../interfaces/user-play";
+import { DataService } from '../data.service';
+import { IRequestPage } from '../../interfaces/request-page';
+import { IReceivedPrize } from '../../interfaces/received-prizes';
+import { IResponsePage } from '../../interfaces/response-page';
+import { IUserPlay } from '../../interfaces/user-play';
+import { IGameHistory } from 'src/app/interfaces/game-history';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GameHistoryService {
-
-  constructor(protected http:DataService) { }
-  request : IRequestPage = new IRequestPage();
-  apiGetReceivedPrizes(msisdn:any){
+  constructor(protected http: DataService) {}
+  request: IRequestPage = new IRequestPage();
+  apiGetReceivedPrizes(msisdn: any) {
     this.request.filtering.msisdn = msisdn;
-    return this.http.post<IResponsePage<IReceivedPrize>>('game-history/received-prize', this.request)
+    return this.http.post<IResponsePage<IReceivedPrize>>(
+      'game-history/received-prize',
+      this.request
+    );
   }
-  apiGetUserInfo(msisdn:any){
-    return this.http.get<IUserPlay>('game-history/user-info', {msisdn})
+  apiGetUserInfo(msisdn: any) {
+    return this.http.get<IUserPlay>('game-history/user-info', { msisdn });
+  }
+
+  apiPlayGame(data: any) {
+    return this.http.post<IGameHistory>('game-history/play', data);
   }
 }
